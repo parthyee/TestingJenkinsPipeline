628ee72e048a50f9b9551d79192ad40e6507a211
parthyee <parthyeekrish@gmail.com>
2021-05-12 16:36:24 +0530

    Update and rename TempFileCreation.ps1 to TempFileCreation

--- TempFileCreation.ps1
+++
@@ -1,8 +0,0 @@
-# Create Temp Directory
-if (-not(Test-Path -Path 'C:\temp'))
-{
-    New-Item -Path 'C:\temp' -ItemType directory
-}
-
-# Using the environment variables exposed by the Jenkins job 
-Set-Content -Path "C:\temp\$($env:Filename).txt" -Value $env:Message
